dcbed7c27aafcd77d9de732b60a2684252b48b77
Gergely, László Géza <Laszlo.Gergely@t-systems.com>
2020-06-12 13:31:04 +0200

    Add project files.

---
+++ WebTutorialRazor/Migrations/20200611151725_ExtendBookToDb.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebTutorialRazor.Migrations
+{
+    public partial class ExtendBookToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Neme",
+                table: "Book");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Author",
+                table: "Book",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ISBN",
+                table: "Book",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Book",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ISBN",
+                table: "Book");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Book");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Author",
+                table: "Book",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Neme",
+                table: "Book",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
---
+++ WebTutorialRazor/Model/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebTutorialRazor.Model
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Name")]
+        public String Name { get; set; }
+
+        public String Author { get; set; }
+
+        public String ISBN { get; set; }
+
+
+    }
+}
